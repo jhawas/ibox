34420cd3415304a4501093a59cfd24402a75bbf6
norman aranez <araneznorman12@gmail.com>
2019-01-24 05:49:40 +0800

    update user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -34,7 +34,6 @@ class CreateUsersTable extends Migration
             $table->string('address')->nullable();
             $table->string('address2')->nullable();
             $table->string('degree')->nullable();
-            $table->string('specialty')->nullable();
             $table->string('password')->nullable();
             $table->string('avatar')->nullable();
             $table->timestamp('email_verified_at')->nullable();
