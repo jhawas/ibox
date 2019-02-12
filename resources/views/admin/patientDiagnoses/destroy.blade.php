8736a33c92da699f6fea0f6c87913877d42da575
Norman Arañez <13890@olinkgroup.com>
2019-02-12 15:59:13 +0800

    remove error

--- resources/views/admin/patientDiagnoses/destroy.blade.php
+++ resources/views/admin/patientDiagnoses/destroy.blade.php
@@ -3,7 +3,7 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h4 class="modal-title" id="myModalLabel">{{ ucfirst($patientDiagnose->diagnose->code) }}</h4>
+        <h4 class="modal-title" id="myModalLabel">{{ ucfirst($patientDiagnose->diagnose ? $patientDiagnose->diagnose->code : null) }}</h4>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
       </div>
       <div class="modal-body">
