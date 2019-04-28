defea8373c525e0b67b232fe14fd8141e640659a
Norman Arañez <13890@olinkgroup.com>
2019-04-29 07:19:12 +0800

    removed unuse feature

--- resources/views/admin/layouts/partials/header.blade.php
+++ resources/views/admin/layouts/partials/header.blade.php
@@ -3,7 +3,7 @@
   <a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
   <!-- Navbar Right Menu-->
   <ul class="app-nav">
-    <li class="app-search">
+    {{-- <li class="app-search">
       <input class="app-search__input" type="search" placeholder="Search">
       <button class="app-search__button"><i class="fa fa-search"></i></button>
     </li>
@@ -47,12 +47,12 @@
         </div>
         <li class="app-notification__footer"><a href="#">See all notifications.</a></li>
       </ul>
-    </li>
+    </li> --}}
     <!-- User Menu-->
     <li class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Open Profile Menu"><i class="fa fa-user fa-lg"></i></a>
       <ul class="dropdown-menu settings-menu dropdown-menu-right">
-        <li><a class="dropdown-item" href="page-user.html"><i class="fa fa-cog fa-lg"></i> Settings</a></li>
-        <li><a class="dropdown-item" href="page-user.html"><i class="fa fa-user fa-lg"></i> Profile</a></li>
+        {{-- <li><a class="dropdown-item" href="page-user.html"><i class="fa fa-cog fa-lg"></i> Settings</a></li>
+        <li><a class="dropdown-item" href="page-user.html"><i class="fa fa-user fa-lg"></i> Profile</a></li> --}}
         <li>
           <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
